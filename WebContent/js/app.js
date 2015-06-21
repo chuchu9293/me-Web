0199530e6bcbb88020c6281b02b0a1e70fe8d620
yc_du <yc_du@Ctrip.com>
2015-06-21 17:57:11 +0800

    gaoKao

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,35 +1,58 @@
-angular.module('qingWebApp', [ 'ngRoute' ]).config(function($routeProvider) {
-	$routeProvider.when('/', {
-		redirectTo : '/countDown'
-	}).when('/gallery', {
-		templateUrl : 'ngViews/gallery.html'
-	}).when('/countDown', {
-		templateUrl : 'countDown.html',
-		controller : 'countDownController'
-	}).when('/menstruation', {
-		templateUrl : 'ngViews/menstruation.html'
-	}).when('/songs', {
-		templateUrl : 'ngViews/songs.html'
-	}).otherwise({
-		redirectTo : '/gallery'
-	});
-	;
-});
+"use strict";
+angular.module('gaoKaoApp', []);
 
-angular.module('qingWebApp').controller("appCtrl",
-/*
-function($scope, $http) {
-	//$http.jsonp期望得到的响应是 angular.callbacks._0(34)
-	$http.jsonp(
-			"http://115.28.88.165/ws/qing/timeSpan"
-					+ "?callback=JSON_CALLBACK").success(
-			function(data) {
-				$scope.timeSpan = data;
-			}).error(function(data) {
-				console.log(data);
-	});*/
-function($scope, $http) {
-	
-}
+angular
+		.module('gaoKaoApp')
+		.controller(
+				"appCtrl",
+				function($scope, $http) {
+					$scope.getScience1 = function() {
+						$http
+								.get(
+										//"http://me.likeyichu.com/webService/gaoKaoScore/science?from=1&to=1")
+								"http://localhost:8080/webService/gaoKaoScore/science?from=1&to=1")
+								.success(function(response) {
+									$scope.science1 = response;
+									$scope.gaoKaoScoreList=$scope.science1;
+								});
+					}//$scope.getScience1
+					
+					$scope.getScience2 = function() {
+						$http
+								.get(
+										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=201&to=400")
+								.success(function(response) {
+									$scope.science2 = response;
+								});
+					}//$scope.getScience2
+					
+					$scope.getScience3 = function() {
+						$http
+								.get(
+										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=401&to=600")
+								.success(function(response) {
+									$scope.science3 = response;
+								});
+					}//$scope.getScience3
+					
+					$scope.getScience4 = function() {
+						$http
+								.get(
+										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=601&to=800")
+								.success(function(response) {
+									$scope.science4 = response;
+								});
+					}//$scope.getScience4
+					
+					$scope.getScience5 = function() {
+						$http
+								.get(
+										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=801&to=1000")
+								.success(function(response) {
+									$scope.science5 = response;
+								});
+					}//$scope.getScience5
+					
+				}
 
-);
+		);
