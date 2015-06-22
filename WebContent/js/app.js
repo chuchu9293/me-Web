c34cfe49bbfc9f729ccc2c133c3b6c702d1c0143
yc_du <yc_du@Ctrip.com>
2015-06-22 16:56:21 +0800

    ha

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -9,8 +9,8 @@ angular
 					$scope.getScience1 = function() {
 						$http
 								.get(
-										//"http://me.likeyichu.com/webService/gaoKaoScore/science?from=1&to=1")
-								"http://localhost:8080/webService/gaoKaoScore/science?from=1&to=1")
+								"http://me.likeyichu.com/webService/gaoKaoScore/science?from=1&to=200")
+								//"http://localhost:8080/webService/gaoKaoScore/science?from=1&to=200")
 								.success(function(response) {
 									$scope.science1 = response;
 									$scope.gaoKaoScoreList=$scope.science1;
@@ -21,8 +21,10 @@ angular
 						$http
 								.get(
 										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=201&to=400")
+								//"http://localhost:8080/webService/gaoKaoScore/science?from=201&to=400")
 								.success(function(response) {
 									$scope.science2 = response;
+									$scope.gaoKaoScoreList=$scope.science2;
 								});
 					}//$scope.getScience2
 					
@@ -32,6 +34,7 @@ angular
 										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=401&to=600")
 								.success(function(response) {
 									$scope.science3 = response;
+									$scope.gaoKaoScoreList=$scope.science3;
 								});
 					}//$scope.getScience3
 					
@@ -41,6 +44,7 @@ angular
 										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=601&to=800")
 								.success(function(response) {
 									$scope.science4 = response;
+									$scope.gaoKaoScoreList=$scope.science4;
 								});
 					}//$scope.getScience4
 					
@@ -50,6 +54,7 @@ angular
 										"http://me.likeyichu.com/webService/gaoKaoScore/science?from=801&to=1000")
 								.success(function(response) {
 									$scope.science5 = response;
+									$scope.gaoKaoScoreList=$scope.science5;
 								});
 					}//$scope.getScience5
 					
